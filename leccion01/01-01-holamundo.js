d097ae0328044b96295145dd955d534dc5c72504
mirenecarmen <ca.ir.ma.80@gmail.com>
2024-05-23 00:38:15 -0300

    primer commit

---
+++ leccion01/01-01-holamundo.js
@@ -0,0 +1,13 @@
+//las variables en javascript son dinamicas
+// el simbolo "=" es el operador de asignacion en javascript
+// para imprimir,en javascripto usamos "console.log(x)",adentro del parentesis va el nombre de 
+//la variable a imprimir.
+//para abrir Quokka hacemos F1 y elegimos new javascript file
+
+
+var nombre = "irene"; //aqui defino el nonbre de la variable
+nombre = "josefa "; //aqui la sobreescribo la variable "nombre" con josefa,y
+// es lo que aparecerá en consola
+var apellido="Machuca";
+console.log(nombre+apellido);
+//console.log(apellido);
---
+++ leccion01/Holamundo.js
@@ -0,0 +1,8 @@
+//se llama "reasignacion" lo que hicimos con la variable "nombre"
+
+//ahora lo que hacemos es apretar F1 y elegimos la opcion Toggle(Star/Stop)oncurrentfile,
+//sirve para arrancar el compilador y tambien para detenerla (apretamos de nuevo F1 y toggle...)
+
+var nombre = "irene";
+nombre = "Hola mundo desde javascript ";
+console.log(nombre);
