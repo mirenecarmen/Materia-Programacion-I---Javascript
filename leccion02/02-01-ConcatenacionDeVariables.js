4a383215b296ad537c7786a1a6ac127d68c4b558
mirenecarmen <ca.ir.ma.80@gmail.com>
2024-06-05 11:57:27 -0300

    similitudes entre js y java-uso del Let

--- leccion02/02-01-ConcatenacionDeVariables.js
+++ leccion02/02-01-ConcatenacionDeVariables.js
@@ -22,3 +22,28 @@ nombre += apellido;
 console.log(nombre);
 
 //ahora vamos al sublimetext
+
+//Hoy ya no se usa var,se una let y const
+//let nombreCompleto2 = "Pedro";
+//console.log(nombreCompleto);
+
+//const apellido2 = "Lepes";
+//apellido2 = "Perez" no puede ser modificada,es una constante
+//console.log(apellido2);
+
+//similitudes con java y javascript
+
+// con let declaramos varias variables dentro de una misma linea 
+//(tambien podemos asignar varias variables en una misma linea) (tambien sucede en java)
+let x,y;
+x = 17 
+y = 21;
+let z = x + y;
+console.log(z);
+// javascript es case sensitive igual que java
+//no se declara una variable con un numero por ejemplo let 1num= 3 o con simbolos
+
+let _1num = 21;
+let rompiendo = "rompe";
+console.log(_1num);
+console.log(rompiendo);
