f45af88ee36f6b4b2aa2a313aacf96fe73f9d7bb
mirenecarmen <ca.ir.ma.80@gmail.com>
2024-06-06 01:08:56 -0300

    funcion if.else en javascript

---
+++ clase7/clase7.js
@@ -0,0 +1,20 @@
+//ejercicio para encontrar numeros pares
+
+let parImpar = 2;
+if(parImpar % 2 == 0){
+    console.log("es un numero PAR");
+}
+else{
+    console.log("es un numero IMPAR");
+}
+
+// ejercicio : es mayor de edad
+
+let edad=15; 
+let adulto=18;
+if (edad >= adulto){
+    console.log("Usted es una persona adulta");
+}
+else{
+    console.log("Usted es una persona menor de edad")
+}
