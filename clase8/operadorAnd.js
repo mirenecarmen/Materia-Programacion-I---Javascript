eac44b6c708bf0097a16aae7e85aaa3ddfd7b1eb
mirenecarmen <ca.ir.ma.80@gmail.com>
2024-06-08 18:58:51 -0300

    Operador logico AND

---
+++ clase8/operadorAnd.js
@@ -0,0 +1,11 @@
+//Usos del operador AND ,se lo representa con el doble anperson &&
+//ambas condiciones deben cumplirse
+
+let dentroRango = 6;
+let valMin = 0;
+let valMax = 10;
+if( dentroRango >= valMin && dentroRango <= valMax) {
+        console.log("esta dentro del rango.");
+} else {
+    console.log("Esta fuera del rango.");
+}
