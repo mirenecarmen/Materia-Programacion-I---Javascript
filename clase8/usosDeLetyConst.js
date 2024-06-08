6746f1fba28ca291de00ec6e3d702be82fe1b806
mirenecarmen <ca.ir.ma.80@gmail.com>
2024-06-08 19:46:53 -0300

    Usos de Let y Const

---
+++ clase8/usosDeLetyConst.js
@@ -0,0 +1,8 @@
+//Hoy ya no se usa VAR,se usa Let y const
+
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const nombre3= "Guadalupe";
+//nombre3 = "-maria" // da error,porque una constante no se puede renombrar
+console.log(nombre3);
